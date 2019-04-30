8ae54c94d0cf995c5f4f3b7b489a3e2159faf82f
Anurag Ruhela <anuragruhela.net@gmail.com>
2019-04-30 17:46:46 +0530

    Added a solution to problem Simple Array Sum.
    https://www.hackerrank.com/challenges/simple-array-sum

---
+++ Warmup/SimpleArraySum.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        int sum = 0;
+        foreach(var i in arr)
+        {
+                sum = sum+i;
+        }
+        Console.WriteLine(sum);
+    }
+}
