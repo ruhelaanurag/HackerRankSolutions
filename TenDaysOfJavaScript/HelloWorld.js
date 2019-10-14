28bfed3cc42baeb7cbb4e130c5e42d063cb0787f
Anurag Ruhela <anuragruhela.net@gmail.com>
2019-10-14 16:33:32 +0530

    Update HelloWorld.js

--- TenDaysOfJavaScript/HelloWorld.js
+++ TenDaysOfJavaScript/HelloWorld.js
@@ -1,1 +1,13 @@
+///Solution to HackerRank problem 
+///Day 0: Hello, World!
+///Problem URL
+///https://www.hackerrank.com/challenges/js10-hello-world/problem
 
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
